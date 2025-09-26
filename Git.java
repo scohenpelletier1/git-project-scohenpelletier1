1d4644b8c05b9491e73f28b1a571ec2a04870571
Sophia <scohenpelletier1@hwemail.com>
2025-09-26 12:22:40 -0700

    (GP-2.1.1): Repository Initialization Testing
    Updated Git.java and added a feature to the initializeRepo() method that verifies repo initialization by calling the new verifyRepoExists() method. Additionally, the resetRepo() method was added, which deletes repositories. Finally, tests were added to verify that resetRepo() works, the README was updated, and bug fixes in initializeRepo() were done.

--- Git.java
+++ Git.java
@@ -2,13 +2,13 @@ import java.io.File;
 
 public class Git {
     public static void initializeRepo(String repoName) {
-        // get all the files
+        // Create the file paths
         File git = new File(repoName);
         File obj = new File(repoName + "/objects");
         File index = new File(repoName + "/index");
         File head = new File(repoName + "/HEAD");
-
-        // check to see if the repo exists
+        
+       // check to see if the repo exists
         if (git.exists() && obj.exists() && index.exists() && head.exists()) {
             System.out.println("Git Repository Already Exists");
             return;
@@ -40,6 +40,73 @@ public class Git {
 
         }
 
+        // check to make sure that the repo exists
+        if (!verifyRepoExists(git)) {
+            System.out.println("Git Repository Could Not Be Created");
+            
+        } else {
+            System.out.println("Git Repository Created");
+        
+        }
+
+    }
+
+    public static void resetRepo(String repoName) {
+        // make sure it exists
+        File git = new File(repoName);
+        if (!verifyRepoExists(git)) {
+            System.out.println("Git Repository Does Not Exist");
+            return;
+
+        }
+
+        // get all the files inside the directory
+        File[] files = git.listFiles();
+
+        // cycle through and delete them
+        for (File file : files) {
+            file.delete();
+        
+        }
+
+        // delete main directory
+        git.delete();
+
+        // check to make sure that the repo was deleted
+        if (verifyRepoExists(git)) {
+            System.out.println("Git Repository Could Not Be Deleted");
+        
+        } else {
+            System.out.println("Git Repository Deleted");
+
+        }
+
+    }
+
+    private static boolean verifyRepoExists(File mainDir) {
+        // make sure the main directory (git) exists
+        if (!mainDir.exists()) {
+            return false;
+        
+        }
+
+        // cycle through all of it's files to make sure that objects, index, and HEAD are there
+        File[] files = mainDir.listFiles();
+
+        for (File file : files) {
+            // if it equals any of the files, return true
+            if ((file.getPath().equals(mainDir.getPath() + "/objects")) ||
+                (file.getPath().equals(mainDir.getPath() + "/index")) ||
+                (file.getPath().equals(mainDir.getPath() + "/HEAD"))) {
+                return true;
+            
+            }
+        
+        }
+
+        // if none of the files exist, return false
+        return false;
+    
     }
 
 }
--- GitTester.java
+++ GitTester.java
@@ -2,11 +2,19 @@ import java.io.FileNotFoundException;
 
 public class GitTester {
     public static void main(String args[]) throws FileNotFoundException {
-        // InitializeRepo tests
+        // initializeRepo tests
         System.out.println("==initializeRepo()==");
         Git.initializeRepo("git"); // Git Repository Created
         Git.initializeRepo("git"); // Git Repository Already Exists
         Git.initializeRepo("git2"); // Git Repository Created
+        Git.initializeRepo("git3"); // Git Repository Created
+        System.out.println();
+
+        // reseteRepo tests
+        System.out.println("==resetRepo()==");
+        Git.resetRepo("git2"); // Git Repository Deleted
+        Git.resetRepo("git3"); // Git Repository Deleted
+        Git.resetRepo("git4"); // Git Repository Does Not Exist
         System.out.println();
 
     }
