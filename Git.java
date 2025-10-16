f68db11e68b996d313c2f391ca1775b33f7c81bd
Sophia Cohen-Pelletier <scohenpelletier1@hwemail.com>
2025-10-15 22:29:32 -0700

    (GP-5.0): Rubric (#8)
    * (GP-5.1): Added tests for init() and add()
    
    Created tests for the GitWrapper methods of init() and add(). I also went into the Git class and fixed the updateIndex() method, which was previously written incorrectly.
    
    * (GP-5.2): Updated the  tester format
    
    Cleaned up the tester file and re-added all my previous tests. Deleted the compression method.
    
    * (GP-5.3): Finished testing trees, updated createTree()
    
    createTree() was not correctly generating the tree, so I fixed this and now it works as expected. Finished creating the tester.

--- Git.java
+++ Git.java
@@ -4,26 +4,24 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 
 public class Git {
-    // instance variables
-    static boolean compression = false; // false by default
-    static String gitRepo;
-    
-    // public methods
+// instance variables
+static String gitRepo;
+
+// public methods
     public static void initializeRepo(String gitRepo) throws IOException {
         Git.gitRepo = gitRepo;
+
         // Create the file paths
         File git = new File(gitRepo);
         File obj = new File(gitRepo + "/objects");
         File index = new File(gitRepo + "/index");
         File head = new File(gitRepo + "/HEAD");
-        
+
         // check to see if the repo exists
         if (git.exists() && obj.exists() && index.exists() && head.exists()) {
             System.out.println("Git Repository Already Exists");
@@ -49,7 +47,6 @@ public class Git {
 
         }
 
-
         // make the head file
         if (!head.exists()) {
             head.createNewFile();
@@ -59,41 +56,42 @@ public class Git {
         // check to make sure that the repo exists
         if (!verifyRepoExists(git)) {
             System.out.println("Git Repository Could Not Be Created");
-            
+
         } else {
             System.out.println("Git Repository Created");
-        
+
         }
 
     }
 
-    public static void resetRepo(String gitRepo) {
+    public static void cleanUp(String filePath) {
         // make sure it exists
-        File git = new File(gitRepo);
-        if (!verifyRepoExists(git)) {
-            System.out.println("Git Repository Does Not Exist");
-            return;
+        File file = new File(filePath);
 
-        }
+        // check if the file is a directory
+        if (file.isDirectory()) {
+            // get all the files inside the directory
+            File[] files = file.listFiles();
 
-        // get all the files inside the directory
-        File[] files = git.listFiles();
+            // cycle through and delete them
+            for (File newFile : files) {
+                if (newFile.isDirectory()) {
+                    cleanUp(newFile.getPath());
+                
+                }
 
-        // cycle through and delete them
-        for (File file : files) {
-            file.delete();
+                newFile.delete();
+
+            }
         
         }
 
-        // delete main directory
-        git.delete();
+        // delete current file
+        file.delete();
 
         // check to make sure that the repo was deleted
-        if (verifyRepoExists(git)) {
+        if (file.exists()) {
             System.out.println("Git Repository Could Not Be Deleted");
-        
-        } else {
-            System.out.println("Git Repository Deleted");
 
         }
 
@@ -114,30 +112,19 @@ public class Git {
 
         for (int i = 0; i < hashBytes.length; i++) {
             hexCode += String.format("%02x", hashBytes[i]);
-        
+
         }
 
         return hexCode;
-    
+
     }
 
     public static String createBlob(String gitRepo, File file) throws NoSuchAlgorithmException, IOException {
         // get the hash
         String hash;
 
-        // if compression is turned on
-        if (compression) {
-            // compress the file and get hash
-            hash = createHash(compressFile(file));
-
-            // get rid of the compressed file
-            compressFile(file).delete();
-
-        } else {
-             // get the file's hash
-            hash = createHash(file);
-        
-        }
+        // get the file's hash
+        hash = createHash(file);
 
         // make the blob file
         File blobFile = new File(gitRepo + "/objects/" + hash);
@@ -157,9 +144,9 @@ public class Git {
             // if the file is there, return true
             if (objectFile.getName().equals(hash)) {
                 return blobFile.getName();
-            
+
             }
-        
+
         }
 
         // Error if the blob file couldn't be created
@@ -168,60 +155,49 @@ public class Git {
 
     }
 
-    public static void updateIndex(File directoryPath) throws NoSuchAlgorithmException, IOException {
-        // get all the files in the folder
-        File[] files = directoryPath.listFiles();
+    public static void updateIndex(File filePath) throws NoSuchAlgorithmException, IOException {
+        // edge cases
+        if (!filePath.exists()) {
+            throw new IllegalArgumentException("File does not exist.");
+
+        } else if (filePath.isDirectory()) {
+            throw new IllegalArgumentException("Cannot add a directory to the index file.");
+
+        }
+
+        // create string builder to make the content that will be added to the index file
         StringBuilder indexContent = new StringBuilder();
 
-        // list all the files
-        for (File file : files) {
-            // if the file is a directory and there are files in the directory
-            if (file.isDirectory() && file.listFiles().length != 0) {
-                // recursively update the index
-                updateIndex(file);
+        // grab everything from the index file, stuff might be replaced
+        indexContent.append(readFile(gitRepo + "/index"));
 
-                // else if it's a file
-            } else if (file.isFile()) {
-                // create the index
-                Git.createBlob(gitRepo, file);
-                indexContent.append(Git.createHash(file) + " " +  file.getPath());
-                indexContent.append("\n");
+        // create blob, then add that to the index file
+        Git.createBlob(gitRepo, filePath);
+        int pathIndex = indexContent.indexOf(filePath.getPath());
 
-            }
+        // if this file already exists in the index
+        if (pathIndex != -1) {
+            // replace it since it's been updated
+            indexContent.replace(pathIndex - 41, pathIndex + filePath.getPath().length(), Git.createHash(filePath) + " " +  filePath.getPath());
+
+        } else {
+            // otherwise just add it normally
+            indexContent.append(Git.createHash(filePath) + " " +  filePath.getPath());
+            indexContent.append("\n");
 
         }
 
         // get the file and buffered writer
-        File index = new File(gitRepo + "/index"); 
-        BufferedWriter writer2 = new BufferedWriter(new FileWriter(index, true));
+        File index = new File(gitRepo + "/index");
+        BufferedWriter writer1 = new BufferedWriter(new FileWriter(index));
 
         // write the index content
-        writer2.write(indexContent.toString());
-        writer2.close();
-    
+        writer1.write(indexContent.toString());
+        writer1.close();
+
     }
 
     // private methods
-    private static File compressFile(File file) throws IOException {
-        // read the original file
-        String fileContents = readFile(file.getPath());
-
-        // encode the contents
-        byte[] bytes = fileContents.getBytes("UTF-8");
-
-        // make a new compressed file
-        File compressedFile = new File("Compressed" + file.getName());
-
-        // get the buffered writer and override the file with the compressed data
-        BufferedWriter writer = new BufferedWriter(new FileWriter(compressedFile));
-        writer.write(bytes.toString());
-        writer.close();
-
-        // return the now compressed file
-        return compressedFile;
-    
-    }
-
     public static String readFile(String path) throws IOException {
         // get the file
         File file = new File(path);
@@ -234,21 +210,21 @@ public class Git {
         // read the file then close the file
         while ((line = reader.readLine()) != null) {
             fileContents += line + "\n";
-        
+
         }
 
         reader.close();
 
         // return the file contents
         return fileContents;
-    
+
     }
 
     private static boolean verifyRepoExists(File mainDir) {
         // make sure the main directory (git) exists
         if (!mainDir.exists()) {
             return false;
-        
+
         }
 
         // cycle through all of it's files to make sure that objects, index, and HEAD are there
@@ -257,17 +233,17 @@ public class Git {
         for (File file : files) {
             // if it equals any of the files, return true
             if ((file.getPath().equals(mainDir.getPath() + "/objects")) ||
-                (file.getPath().equals(mainDir.getPath() + "/index")) ||
-                (file.getPath().equals(mainDir.getPath() + "/HEAD"))) {
+            (file.getPath().equals(mainDir.getPath() + "/index")) ||
+            (file.getPath().equals(mainDir.getPath() + "/HEAD"))) {
                 return true;
-            
+
             }
-        
+
         }
 
         // if none of the files exist, return false
         return false;
-    
+
     }
 
 }
--- GitTester.java
+++ GitTester.java
@@ -32,104 +32,216 @@ public class GitTester {
     public static void resetEverything(String repoName) throws IOException {
         // deletes all files
         resetObjectFiles(repoName);
-        Git.resetRepo(repoName);
+        Git.cleanUp(repoName);
 
         // remakes the core components of the repo
         Git.initializeRepo(repoName);
     
     }
 
-    public static void main(String args[]) throws IOException, NoSuchAlgorithmException {
+    public static void testInit(GitWrapper gw) {
+        try {
+            // create repo
+            gw.init();
+
+            // delete objects and see if code still works
+            File obj = new File("git/objects");
+            obj.delete();
+
+            gw.init();
+
+            // delete index and HEAD and see if code still works
+            File index = new File("git/index");
+            File head = new File("git/HEAD");
+            index.delete();
+            head.delete();
+
+            gw.init();
+
+            // make sure running it again doesn't cause errors
+            gw.init();
+            
+        } catch (Exception e) {
+            System.out.println("init() does not work");
+
+        }
+    
+    }
+
+    public static void testAddEdge(GitWrapper gw) {
+        try {
+            // throws error for a nonexistant path
+            gw.add("myProgram/inner/world.txt");
+
+        } catch (Exception e) {
+            System.out.println("File does not exist.");
+
+        }
+
+        try {
+            // throws error for directory path
+            gw.add("myProgram/scripts");
+
+        } catch (Exception e) {
+            System.out.println("Cannot add a directory to the index file.");
+
+        }
+    
+    }
+
+    public static void testAdd(GitWrapper gw) {
+        try {
+            // stages a new file
+            gw.add("myProgram/hello.txt");
+            System.out.println("myProgram/hello.txt added to index file.");
+
+            if (new File("git/objects/" + Git.createHash(new File("myProgram/hello.txt"))).exists()) {
+                System.out.println("Blob for this file was created");
+            
+            } else {
+                System.out.println("FAILED TO CREATE BLOB");
+            
+            }
+
+            // make change, stage again
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("myProgram/hello.txt"))) {
+                writer.write("I have updated the file myProgram/hello.txt to say this instead of \"hi\"");
+            }
+
+            gw.add("myProgram/hello.txt");
+            System.out.println("myProgram/hello.txt changed and re-added to index file.");
+
+            if (new File("git/objects/" + Git.createHash(new File("myProgram/hello.txt"))).exists()) {
+                System.out.println("Blob for this file was updated, two versions now exist in git/objects");
+            
+            } else {
+                System.out.println("FAILED TO CREATE NEW BLOB");
+            
+            }
+
+            // no update means nothing changes
+            gw.add("myProgram/hello.txt");
+            System.out.println("myProgram/hello.txt unchanged and not re-added to index file.");
+            
+        } catch (Exception e) {
+            System.out.println("add() does not work");
+
+        }
+    
+    }
+    
+    public static void main(String args[]) throws IOException, NoSuchAlgorithmException {        
         // initializeRepo tests
         System.out.println("==initializeRepo()==");
-        // Git.initializeRepo("git1"); // Git Repository Created
-        // Git.initializeRepo("git1"); // Git Repository Already Exists
-        // Git.initializeRepo("git2"); // Git Repository Created
+        Git.initializeRepo("git1"); // Git Repository Created
+        Git.initializeRepo("git1"); // Git Repository Already Exists
+        Git.initializeRepo("git2"); // Git Repository Created
         Git.initializeRepo("git"); // Git Repository Created
 
-        // add textFile folder
+        // add textFile folder and a bunch of files and directories
         File myProgram = new File("myProgram");
+        myProgram.mkdir();
+
+        File scripts = new File("myProgram/scripts");
+        scripts.mkdir();
+
+        File file1 = createFile("myProgram/file1.txt", "Hello, World!");
+        File file2 = createFile("myProgram/file2.txt", "My name is Sophia Cohen-Pelletier :D");
+        File file3 = createFile("myProgram/file3.txt", 
+                                "Why are you still reading the test cases?");
+        File sample1 = new File("myProgram/scripts/sample1.txt");
+        File sample2 = new File("myProgram/scripts/sample2.txt");
+        
+        file1.createNewFile();
+        file2.createNewFile();
+        file3.createNewFile();
+        sample1.createNewFile();
+        sample2.createNewFile();
 
         System.out.println();
 
-        // // reseteRepo tests
-        // System.out.println("==resetRepo()==");
-        // Git.resetRepo("git1"); // Git Repository Deleted
-        // Git.resetRepo("git2"); // Git Repository Deleted
-        // Git.resetRepo("git4"); // Git Repository Does Not Exist
-        // System.out.println();
-
-        // // createHash tests
-        // System.out.println("==createHash()==");
-        // File file1 = createFile("myProgram/file1.txt", "Hello, World!");
-        // System.out.println(Git.createHash(file1)); // 0a0a9f2a6772942557ab5355d76af442f8f65e01
-        // File file2 = createFile("myProgram/file2.txt", "My name is Sophia Cohen-Pelletier :D");
-        // System.out.println(Git.createHash(file2)); // 82a593ef07d35285dd53c050a5cc564709b07dab
-        // File file3 = createFile("myProgram/file3.txt", "Why are you still reading the test cases?");
-        // System.out.println(Git.createHash(file3)); // 2020d57460bdc7624d7e0e746b746cfa81414be5
-        // System.out.println();
-
-        // // createBlob tests
-        // System.out.println("==createBlob()=="); // everything should return true
-
-        // // compression on
-        // Git.compression = true;
-        // System.out.println(Git.createBlob("git", file1)); // creates file 320ff6a9534a245d34a1f15b7affa021fb53301a
-        // System.out.println(Git.createBlob("git", file2)); // creates file 1192dca5eb1608edb89cee23186f5fd2bf793af1
-        // System.out.println(Git.createBlob("git", file3)); // creates file cf9eef560135abb820422435cf2be5b4089adba5
-        // System.out.println();
-
-        // resetObjectFiles("git");
-
-        // // compression off
-        // Git.compression = false;
-        // System.out.println(Git.createBlob("git", file1)); // creates file 0a0a9f2a6772942557ab5355d76af442f8f65e01
-        // System.out.println(Git.createBlob("git", file2)); // creates file 82a593ef07d35285dd53c050a5cc564709b07dab
-        // System.out.println(Git.createBlob("git", file3)); // creates file 2020d57460bdc7624d7e0e746b746cfa81414be5
-
-        // // updateIndex tests
-        // System.out.println("==updateIndex()==");
-        // Git.updateIndex("git", file1); // db1668952fdb286939fc39d573ed88c720323b69 git/file1
-        // Git.updateIndex("git", file2); // f3db729468c3b8ff98e9d88a313d5dda633d26f7 git/file2
-        // Git.updateIndex("git", file3); // 1a282e683577b87e845d8f197ad2a7c7bda15384 git/file3
-
-        // // check to see if files inside of git work
-        // File hello = createFile("git/hello.txt", "hi");
-        // Git.updateIndex("git", hello);
-        // System.out.println();
-
-        // // check for duplicates
-        // File helloCopy = createFile("myProgram/helloCopy.txt", "hi");
-        // File helloCopy2 = createFile("myProgram/helloCopy.txt", "hi");
-        // File hello2 = createFile("myProgram/hello.txt", "hi");
-
-        // Git.updateIndex("git", helloCopy);
-        // Git.updateIndex("git", helloCopy2);
-        // Git.updateIndex("git", hello2);
-
-        // // files can be modified
-        // BufferedWriter writer = new BufferedWriter(new FileWriter(hello));
-        // writer.write("HIII!!");
-        // writer.close();
-        // Git.updateIndex("git", hello);
-
-        System.out.println("==resetEverything()==");
-        // resetEverything
-        resetEverything("git");
+        // reseteRepo tests
+        System.out.println("==resetRepo()==");
+        Git.cleanUp("git1"); // Git Repository Deleted
+        Git.cleanUp("git2"); // Git Repository Deleted
+        Git.cleanUp("git4"); // Git Repository Does Not Exist
         System.out.println();
+
+        // createHash tests
+        System.out.println("==createHash()==");
+        System.out.println(Git.createHash(file1)); // 60fde9c2310b0d4cad4dab8d126b04387efba289
+        System.out.println(Git.createHash(file2)); // 8e51dbad52781f72f37a61360ece0d0f470fd23d
+        System.out.println(Git.createHash(file3)); // fcc0c9bae1e2361a702a318bb16a8e87d23c4f2d
+        System.out.println();
+
+        // createBlob tests
+        System.out.println("==createBlob()==");
+        System.out.println(Git.createBlob("git", file1)); // 60fde9c2310b0d4cad4dab8d126b04387efba289
+        System.out.println(Git.createBlob("git", file2)); // 8e51dbad52781f72f37a61360ece0d0f470fd23d
+        System.out.println(Git.createBlob("git", file3)); // fcc0c9bae1e2361a702a318bb16a8e87d23c4f2d
+
+        // now with files with the same content
+        System.out.println(Git.createBlob("git", sample1)); // da39a3ee5e6b4b0d3255bfef95601890afd80709
+        System.out.println(Git.createBlob("git", sample2)); // da39a3ee5e6b4b0d3255bfef95601890afd80709
+        System.out.println();
+
+        // updateIndex tests
+        System.out.println("==updateIndex()==");
+        Git.updateIndex(file1);
+        Git.updateIndex(file2);
+        Git.updateIndex(file3);
+        Git.updateIndex(sample1);
+        Git.updateIndex(sample2);
+        System.out.println();
+
+        // check for if duplicates work
+        File hello = createFile("myProgram/hello.txt", "hi");
+        File helloCopy = createFile("myProgram/helloCopy.txt", "hi");
+        File helloCopy2 = createFile("myProgram/helloCopy.txt", "hi");
+
+        Git.updateIndex(hello);
+        Git.updateIndex(helloCopy);
+        Git.updateIndex(helloCopy2);
+
+        // files can be modified
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(hello))) {
+            writer.write("HIII!!");
+        }
+
+        Git.updateIndex(hello);
 
         System.out.println("==createTree()==");
         System.out.println(Tree.createTree(myProgram));
         System.out.println();
         
-        System.out.println("==resetEverything()=="); // again
-        // resetEverything
-        resetEverything("git");
+        // reset object files
+        resetObjectFiles("git");
+
+        // works multiple times, same outcome
+        System.out.println("==createIndexTree()==");
+        System.out.println(Tree.createIndexTree());
+        System.out.println(Tree.createIndexTree());
+        System.out.println(Tree.createIndexTree());
         System.out.println();
 
-        System.out.println("==createIndexTree()==");
-        Git.updateIndex(myProgram);
-        System.out.println(Tree.createIndexTree(myProgram));
+        // delete git
+        Git.cleanUp("git");
+
+        // GP-5.0 TESTS vvvvvvvv
+
+        // wrapper tests
+        GitWrapper gw = new GitWrapper();
+
+        // init()
+        System.out.println("==init()==");
+        testInit(gw);
+        System.out.println();
+
+        // add()
+        System.out.println("==add()==");
+        testAddEdge(gw);
+        System.out.println();
+        testAdd(gw);
         System.out.println();
 
     }
